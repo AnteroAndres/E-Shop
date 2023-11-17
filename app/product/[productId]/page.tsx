3e1e05ce591108513061379b14e52e192774b57f
Antero Montalvo <antero2203@gmail.com>
2023-11-17 15:27:46 -0500

    avatar component

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,6 +1,7 @@
 import Container from "@/app/components/Container";
 import { product } from "@/utils/product";
 import ProductDetails from "./ProductDetails";
+import ListRating from "./ListRating";
 
 interface IPrams {
     productId?: string
@@ -12,6 +13,10 @@ const Product = ({params}:{params:IPrams}) => {
     return <div className="p-8">
     <Container>
             <ProductDetails product={product}/>
+            <div className="flex flex-col mt-20 gap-4">
+                <div>Add Rating</div>
+                <ListRating product={product}/>
+            </div>
     </Container>
     </div>
 }
